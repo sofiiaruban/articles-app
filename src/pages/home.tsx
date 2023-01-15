e72bd2c9c0d3bf4ab1bafd2eb32cf464823a09b8
Sofia Ruban <sophiaruban249324@gmail.com>
2023-01-15 21:32:44 +0200

    style: Add search icon

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,31 @@
+import { InputAdornment } from "@mui/material"
+import FormControl from "@mui/material/FormControl"
+import InputLabel from "@mui/material/InputLabel"
+import TextField from "@mui/material/TextField"
+import { SearchIcon } from "../assets/icons/SearchIcon"
 
 
 export const Home = () => {
     return (
         <>
-        <label htmlFor="article">Filter by keywords</label>
-        <input type="text" name="article"/>
+        <FormControl>
+        <InputLabel shrink htmlFor="outlined-basic">
+        Filter by keywords
+        </InputLabel>
+            <TextField
+              id="outlined-basic"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SearchIcon />
+                  </InputAdornment>
+                ),
+              }}
+              variant="outlined"/>
+        </FormControl>
+        
+        
+        
 
         </>
     )
