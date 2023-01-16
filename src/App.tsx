24b3eee1cbb65c843a6a4937eb4c58dbd94053aa
Sofia Ruban <sophiaruban249324@gmail.com>
2023-01-16 20:21:25 +0200

    refactor: Rename home and article info components

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,18 @@ import Box from '@mui/material/Box';
 import React, { FC } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import styles from './App.module.scss';
-import { ArticleInfo } from './pages/articleInfo';
-import { Home } from './pages/home';
+import { ArticleInfo } from '/pages/ArticleInfo';
+import { Home } from './pages/Home';
 //import { useGetArticleByIdQuery, useGetArticlesQuery } from './services/articleApi';
 
 
 const  App:FC = () => {
   return (
     <Box className={styles.box}>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="article/:id" element={<ArticleInfo/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="article/:id" element={<ArticleInfo/>}/>
+      </Routes>
     </Box>
   );
 }
