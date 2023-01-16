d008728e398c876f6b805e4baf2289466c20c59f
Sofia Ruban <sophiaruban249324@gmail.com>
2023-01-16 19:47:31 +0200

    refactor (article component):  Rename component from Article to ArticleCard

--- src/App.tsx
+++ src/App.tsx
@@ -4,33 +4,16 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import styles from './App.module.scss';
 import { ArticleInfo } from './pages/articleInfo';
 import { Home } from './pages/home';
-import { useGetArticleByIdQuery, useGetArticlesQuery } from './services/articleApi';
+//import { useGetArticleByIdQuery, useGetArticlesQuery } from './services/articleApi';
 
-const  App:FC = () =>{
-  const id: number = 17895;
-  const {data, error, isLoading} = useGetArticleByIdQuery(id); 
-
-  if (isLoading) {
-    return (
-      <div>
-        Feed loading...
-      </div>
-    )
-  }
-  if (error) {
-    return (
-      <div>
-        Error while loading feed
-      </div>
-    )
-  }
+const  App:FC = () => {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="article/:id" element={<ArticleInfo/>}/>
-        </Routes>
-      </BrowserRouter>
+    <Box className={styles.box}>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="article/:id" element={<ArticleInfo/>}/>
+    </Routes>
+    </Box>
   );
 }
 
---
+++ src/components/articleCard.tsx
@@ -0,0 +1,6 @@
+
+export const ArticleCard = () => {
+    return (
+        <div className="">Article</div>
+    )
+}
