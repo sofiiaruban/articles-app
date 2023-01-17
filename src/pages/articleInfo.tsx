1cbd24d8fd31c1738c5bdb6ac280c1052ff6098f
Sofia Ruban <sophiaruban249324@gmail.com>
2023-01-17 20:38:52 +0200

    feat(articleInfo): Add back button

--- src/pages/articleInfo.tsx
+++ src/pages/articleInfo.tsx
@@ -1,9 +1,12 @@
+import Button from "@mui/material/Button";
+import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
+import { useNavigate } from "react-router-dom";
 import { useGetArticleByIdQuery } from "../services/articleApi"
 
 export const ArticleInfo = ({id}:{id:number}) => {
-   
   const {data, error, isLoading} = useGetArticleByIdQuery(id);
+  const navigate = useNavigate();
 
   if(isLoading) {
     return (<Typography>Loading...</Typography>) 
@@ -16,13 +19,14 @@ export const ArticleInfo = ({id}:{id:number}) => {
   return (
     <>
       <CardMedia
-          className={}
+          className=""
           component="img"
-          image={imgUrl}
+          image={data?.imageUrl}
           alt="article"
         />
       <Typography>{data?.title}</Typography>
       <Typography>{data?.summary}</Typography>
+      <Button size="small" color="primary" onClick={()=> navigate('/', {replace: true})}>Back to homepage</Button>
     </>
   )
 
