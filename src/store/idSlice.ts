b52f92434484b824a63961d9734e8e00dd9533b1
Sofia Ruban <sophiaruban249324@gmail.com>
2023-01-17 18:36:59 +0200

    feat(idSlice): Add idSlice

--- src/store/idSlice.ts
+++ src/store/idSlice.ts
@@ -1,11 +1,21 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { IdState } from '../types/types';
-
+import { RootState } from './store';
 
 const initialState: IdState = {
    id: 0
 }
 
 export const idSlice = createSlice(
-    
-)
+  { 
+  name:"id",
+  initialState,
+  reducers: {
+    updateId: (state, action: PayloadAction<number>) => {
+      state.id = action.payload;
+    }
+  } 
+})
+export const { updateId } =  idSlice.actions
+
+export default idSlice.reducer
