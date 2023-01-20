19fb668c0991ff5856ad6d8d382a6b8241eeff7a
Sofia Ruban <sophiaruban249324@gmail.com>
2023-01-20 17:34:58 +0200

    refactor(published data): Remove fullDate variable

--- src/components/PublishedDate.tsx
+++ src/components/PublishedDate.tsx
@@ -3,10 +3,9 @@ import Typography from '@mui/material/Typography';
 
 export const PublishedDate = ({date}: {date: string})=>{
     const monthList = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    const fullDate=date;
-    let year =fullDate.slice(0,4);
-    let month = fullDate.slice(5,7);
-    let day = fullDate.slice(8,10);
+    let year = date.slice(0,4);
+    let month = date.slice(5,7);
+    let day = date.slice(8,10);
 
     return (
         <Typography  variant="body1">{`${monthList[(+month)-1]} ${day}th, ${year}`}</Typography>
