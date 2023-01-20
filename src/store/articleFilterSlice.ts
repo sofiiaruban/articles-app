ceccc6a776b7a20bbbe34f5768b13c61fe235acd
Sofia Ruban <sophiaruban249324@gmail.com>
2023-01-20 18:51:37 +0200

    refactor: Delete unused imports

--- src/store/articleFilterSlice.ts
+++ src/store/articleFilterSlice.ts
@@ -1,6 +1,5 @@
 
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { useGetArticlesQuery } from '../services/articleApi';
 import { ArticleItems, FilterStates} from '../types/types';
 
 
